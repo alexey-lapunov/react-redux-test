f47b46ab3fe590fb152aa213af5fcae41c9be5e9
Alexey <leshka.lapunov@gmail.com>
2019-10-21 23:29:07 +0300

    finished stylization PhotoSlider component

--- src/components/PhotoSlider/PhotoSlider.js
+++ src/components/PhotoSlider/PhotoSlider.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 
 import Swiper from 'react-id-swiper';
 import 'react-id-swiper/lib/styles/scss/swiper.scss';
 
 import styles from './styles.module.scss';
 
-const SwiperSlider = ({ photos = [] }) => {
+const SwiperSlider = ({ photos }) => {
+  const [swiper, updateSwiper] = useState(null);
+
+  const goNext = () => {
+    if (swiper !== null) {
+      swiper.slideNext();
+    }
+  };
+
+  const goPrev = () => {
+    if (swiper !== null) {
+      swiper.slidePrev();
+    }
+  };
+
   return (
     <div className={styles.box}>
       <div className={styles.content}>
-        <div className={styles.slider}>
-          <Swiper>
-            {photos.map(photo => (
-              <div key={photo.id} className={styles.slide}>
-                <img
-                  className={styles.picture}
-                  src={photo.url}
-                  alt={photo.title}
-                />
-              </div>
-            ))}
-          </Swiper>
-        </div>
+        {photos.length ? (
+          <div className={styles.slider}>
+            <button
+              className={classnames(styles.button, styles.buttonPrev)}
+              onClick={goPrev}
+            >
+              Prev
+            </button>
+            <button
+              className={classnames(styles.button, styles.buttonNext)}
+              onClick={goNext}
+            >
+              Next
+            </button>
+            <Swiper getSwiper={updateSwiper}>
+              {photos.map(photo => (
+                <div key={photo.id} className={styles.slide}>
+                  <img
+                    className={styles.picture}
+                    src={photo.url}
+                    alt={photo.title}
+                  />
+                </div>
+              ))}
+            </Swiper>
+          </div>
+        ) : (
+          <span>Loading...</span>
+        )}
       </div>
     </div>
   );
