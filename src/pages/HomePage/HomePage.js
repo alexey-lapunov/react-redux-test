57be54203846ac5ac95f40b51dab06d33d8c229c
Alexey <leshka.lapunov@gmail.com>
2019-10-31 23:10:23 +0300

    finish project

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,9 +5,12 @@ import styles from './styles.module.scss';
 const HomePage = () => {
   return (
     <div className={styles.home}>
-      <h1 className={styles.title}>
-        <span>Welcome to my</span> React-Redux application =)
-      </h1>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          <span>Welcome to my</span> React-Redux application =)
+        </h1>
+        <span className={styles.desc}>Click on the burger menu</span>
+      </div>
     </div>
   );
 };
